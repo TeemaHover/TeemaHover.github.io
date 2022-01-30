bd6a1ed2a5c29e5040d428449a4541c177d651ed
fl1pHover <bishu.hover@gmail.com>
2022-01-30 22:55:22 +0800

    asd

--- js/java.js
+++
@@ -1,5 +0,0 @@
-// const openMenu = document.querySelector(".open__menu");
-// const closeMenu = document.querySelector(".close__menu");
-
-// openMenu.addEventListener("click", show);
-// closeMenu.addEventListener("click", close);
